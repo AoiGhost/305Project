0849538e3022ad74046378bb4682de4c2a060a87
Memeedo <starxxx4010@gmail.com>
2023-12-02 23:11:35 +0300

    Update Main.java

--- OnlineLibrary/src/pkg305groupproject/Main.java
+++ OnlineLibrary/src/pkg305groupproject/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         //I have added changes
         System.out.println("x");
+        System.out.println("yoyo,glg");
+        
     }
     
 }
