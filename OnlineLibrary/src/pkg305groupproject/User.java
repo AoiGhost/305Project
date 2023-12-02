bcc9427b1cfbb01d3876253ba597c002c255272f
Memeedo <starxxx4010@gmail.com>
2023-12-03 00:36:22 +0300

    Create User.java

---
+++ OnlineLibrary/src/pkg305groupproject/User.java
@@ -0,0 +1,32 @@
+
+package pkg305groupproject;
+
+
+public class User {
+    private int ID;
+    private String password;
+
+    public User(int ID, String password) {
+        this.ID = ID;
+        this.password = password;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+               
+}
