b21f242aaedd575c582ea76f4f61dadcf5bace1e
AoiGhost <aoighostchan@gmail.com>
2023-12-03 12:34:55 +0300

    Create Git_Pull.bat

---
+++ Git_Pull.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM Set the path to your local repository
+set REPO_PATH=C:\Users\96657\Documents\GitHub\305Project
+
+REM Navigate to the repository
+cd /d %REPO_PATH%
+
+REM pull all changes from the git repo 
+git pull
+
