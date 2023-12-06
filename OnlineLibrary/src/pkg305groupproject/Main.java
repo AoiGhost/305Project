0af9abc179fec03ca33b9ce1db09c0fc2cb93944
AoiGhost <aoighostchan@gmail.com>
2023-12-06 22:33:37 +0300

    f
    fff

--- OnlineLibrary/src/pkg305groupproject/Main.java
+++ OnlineLibrary/src/pkg305groupproject/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         Book books = new Book();
-        ClientHandler.RentedBook(1, 3);
+        System.out.println(ClientHandler.RentedBook(1, 3));
         
         
         
