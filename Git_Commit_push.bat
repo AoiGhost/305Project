2c72aa8b44e32d644de5aa86c225c11f4aadb757
AoiGhost <aoighostchan@gmail.com>
2023-12-07 03:09:04 +0300

    Automated commit via script

--- Git_Commit_push.bat
+++ Git_Commit_push.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM Set the path to your local repository
-set REPO_PATH=C:\Users\zezox\Documents\GitHub\305Project
+set REPO_PATH=C:\Users\96657\Documents\GitHub\305Project
 
 REM Navigate to the repository
 cd /d %REPO_PATH%
