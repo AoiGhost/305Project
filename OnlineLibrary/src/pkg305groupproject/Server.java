98f24548535613679e3b7259cc6f61faaa21baf1
AoiGhost <aoighostchan@gmail.com>
2023-12-07 18:11:01 +0300

    Update Server.java

--- OnlineLibrary/src/pkg305groupproject/Server.java
+++ OnlineLibrary/src/pkg305groupproject/Server.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) throws IOException {
         System.out.println(ClientHandler.RentedBook(1, 3));
-      
+        
         
     }
     
