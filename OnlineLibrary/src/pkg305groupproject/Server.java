e6da915d68e8618c6b8b86908f9fffc61a2833d3
AoiGhost <aoighostchan@gmail.com>
2023-12-07 17:53:11 +0300

    Automated commit via script

--- OnlineLibrary/src/pkg305groupproject/Server.java
+++ OnlineLibrary/src/pkg305groupproject/Server.java
@@ -7,26 +7,8 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        
-        try  {
-            DatabaseConnection DB = new DatabaseConnection();
-            System.out.println("Server on, Waiting for clients....");
-            int Client_Number=1;
-            ServerSocket server_socket = new ServerSocket(9324);
-            while (true) {                
-                Socket incoming = server_socket.accept();
-                System.out.println("Clinet Number: "+ Client_Number);
-                Runnable r = new ClientHandler(incoming);
-                Thread th = new Thread(r);
-                th.start();
-                th.join();
-                
-                Client_Number++;
-             
-            }
-        } catch(IOException | InterruptedException e){
-        
-        }
+        System.out.println(ClientHandler.RentedBook(4, 3));
+      
         
     }
     
